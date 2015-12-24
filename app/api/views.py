54c99b0899ea680fbb76ecf9eedc064f815a7c9b
jakubjanuzik <jakubjanuzik@gmail.com>
2015-12-24 12:22:57 +0100

    Fixes to SQL, added example usage

--- app/api/views.py
+++ app/api/views.py
@@ -13,7 +13,18 @@ def hello():
 
 @api.route('/all/<string:tablename>', methods=['GET'])
 def get_all_from_table(tablename):
-    data = utils.select_all_from_table(tablename)
+    # For testing purposes :-) Example usage
+
+    # values = {'id': 6, 'name': 'qwert'}
+    # utils.insert_into_table(tablename, values)
+    # data = utils.select_all_from_table(tablename)
+
+    data = utils.select_fields_from_table_by_ids(
+        tablename=tablename,
+        fields=['id', 'name'],
+        ids_list=[2, 3, 4]
+    )
+
     return jsonify({
         'error': False,
         'result': data
