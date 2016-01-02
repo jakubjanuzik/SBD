1923a6c90d5bd20b763560caca0c64d0ba02fbc5
jakubjanuzik <jakubjanuzik@gmail.com>
2016-01-02 19:52:08 +0100

    Removal, edition, list

--- app/clients/models.py
+++ app/clients/models.py
@@ -1,25 +1,54 @@
 from collections import namedtuple
+
+from flask import abort
+from psycopg2 import ProgrammingError
 from app import utils
 
 
 def create_client(form):
-    clientData = {
+    client_data = {
         'name': form.data['name'],
         'surname': form.data['surname'],
         'email': form.data['email']
     }
 
-    id = utils.insert('clients', clientData)
-    for x in form.data["phones"]:
-        utils.insert('client_phones', {'client_id': id, 'phone': x['phone']})
+    client_id = utils.insert('clients', client_data)
+    for phone in form.data['phones']:
+        utils.insert(
+            'client_phones', {'client_id': client_id, 'phone': phone['phone']}
+        )
 
 
-def get_client(id, abort=True):
+def edit_client(form, client_id):
+    client_data = {
+        'name': form.data['name'],
+        'surname': form.data['surname'],
+        'email': form.data['email'],
+    }
+
+    utils.run_custom_query(
+            """DELETE FROM CLIENT_PHONES WHERE CLIENT_ID = {}""".format(
+                client_id
+            ),
+            fetch=False
+        )
+    utils.update('clients', {'id': client_id},  client_data)
+    for phone in form.data['phones']:
+        utils.insert(
+            'client_phones',
+            {'client_id': client_id, 'phone': phone['phone']}
+        )
+
+
+def get_client(id):
     client = utils.select_one_or_404('clients', {'id': id})
     phones = utils.select('client_phones', {'client_id': id})
 
-    Client = namedtuple('Client', ['name', 'surname', 'email', 'phones'])
+    Client = namedtuple(
+        'Client', ['id', 'name', 'surname', 'email', 'phones']
+    )
     client = Client(
+        id=client.id,
         name=client.name,
         surname=client.surname,
         email=client.email,
--- app/clients/views.py
+++ app/clients/views.py
@@ -1,8 +1,10 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.decorators import login_required
+from app.utils import run_custom_query
+
 from . import clients
 from .forms import UserForm
-from app.decorators import login_required
-from .models import create_client, get_client
+from .models import create_client, get_client, edit_client
 
 
 @login_required
@@ -15,16 +17,47 @@ def create():
 
 
 @login_required
-@clients.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    client = get_client(id)
+@clients.route('/edit/<int:client_id>', methods=['GET', 'POST'])
+def edit(client_id):
+    client = get_client(client_id)
     form = UserForm(obj=client)
     if form.validate_on_submit():
-        create_client(form)
+        edit_client(form, client_id)
+        redirect(url_for('clients.list'))
     return render_template('clients/create.html', form=form)
 
 
 @login_required
 @clients.route('/', methods=['GET'])
 def list():
-    return render_template('clients/list.html')
+    clients = run_custom_query(
+        """SELECT * FROM clients"""
+    )
+    clients_list = []
+    for client in clients:
+        clients_list.append(get_client(client.id))
+
+    return render_template(
+        'clients/list.html',
+        clients=sorted(clients_list, key=lambda x: x.id)
+    )
+
+
+@login_required
+@clients.route('/delete/<int:client_id>', methods=['GET'])
+def delete(client_id):
+    try:
+        run_custom_query(
+            """DELETE FROM CLIENT_PHONES WHERE client_id = {}""".format(
+                client_id
+            ),
+            fetch=False
+        )
+        run_custom_query(
+            """DELETE FROM CLIENTS WHERE ID = {}""".format(client_id),
+            fetch=False
+        )
+        return redirect(url_for('clients.list'))
+    except:
+        flash('Something went wrong. Please, contact administrator', 'error')
+        return redirect(url_for('clients.list'))
