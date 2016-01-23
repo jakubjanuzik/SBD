dd25d08d9845df99a856846176c0c307afdfd099
jakubjanuzik <jakubjanuzik@gmail.com>
2016-01-23 10:56:36 +0100

    Added fixtures to db.sql

--- db.sql
+++ db.sql
@@ -1070,3 +1070,16 @@ REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
+
+INSERT INTO users (username, password)
+values ('sbd', 'b98d50c159a938723d8eb8f3039afab2');
+
+
+INSERT INTO order_statuses (id, status_name)
+values (1, 'New');
+
+INSERT INTO order_statuses (id, status_name)
+values (2, 'Cancelled');
+
+INSERT INTO order_statuses (id, status_name)
+values (3, 'Confirmed');
